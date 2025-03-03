50cbff8fa470051d523a023691f9c856565df3a7
Fadia <guelbifedia@gmail.com>
2025-03-03 11:36:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+
+
+// Affichage des noms avec différentes couleurs
+console.log(chalk.bgRed.bold('Coralie'));
+console.log(chalk.bgCyan ('Galystan'));
+console.log(chalk.underline.blueBright ('David'));
+console.log(chalk.italic.bgMagenta.bold ('Lucie'));
